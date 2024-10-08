c5e13e4cb6979bbb25b0800b93d1d9ac036df94b
alexbs01 <alex.becerra.suarez01@gmail.com>
2024-10-08 12:07:42 +0200

    El proyecto ya vuelve a funcionar

--- appOlimpiadas/frontendWeb/src/components/table/Table.jsx
+++ appOlimpiadas/frontendWeb/src/components/table/Table.jsx
@@ -33,34 +33,10 @@ function TablePaises({ data }) {
                 pageSize={5}
                 rowsPerPageOptions={[5]}
                 checkboxSelection
+                disableMultipleRowSelection
                 disableSelectionOnClick
             />
         </Paper>
-    );
-}
-
-function TablePaises2({ data }) {
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>ID_PAIS</th>
-                    <th>NOMBRE_PAIS</th>
-                    <th>CODIGO_PAIS</th>
-                    <th>VALOR_PAIS</th>
-                </tr>
-            </thead>
-            <tbody>
-                {data.map((element, index) => (
-                    <tr key={index}>
-                        <td>{element.ID_PAIS}</td>
-                        <td>{element.NOMBRE_PAIS}</td>
-                        <td>{element.CODIGO_PAIS}</td>
-                        <td>{element.VALOR_PAIS}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
     );
 }
 
@@ -93,39 +69,9 @@ function TableSedes({ data }) {
                 columns={columns}
                 pageSize={5}
                 rowsPerPageOptions={[5]}
-                checkboxSelection
                 disableSelectionOnClick
             />
         </Paper>
-    );
-}
-
-function TableSedes1({ data }) {
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>ID_PAIS</th>
-                    <th>NOMBRE_PAIS</th>
-                    <th>ID_CIUDAD</th>
-                    <th>VALOR_CIUDAD</th>
-                    <th>DESCRIPCION_TIPO</th>
-                    <th>COUNT_SEDES</th>
-                </tr>
-            </thead>
-            <tbody>
-                {data.map((element, index) => (
-                    <tr key={index}>
-                        <td>{element.ID_PAIS}</td>
-                        <td>{element.NOMBRE_PAIS}</td>
-                        <td>{element.ID_CIUDAD}</td>
-                        <td>{element.VALOR_CIUDAD}</td>
-                        <td>{element.DESCRIPCION_TIPO}</td>
-                        <td>{element.COUNT_SEDES}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
     );
 }
 
