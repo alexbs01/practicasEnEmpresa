a5ae7f3fa7518b9957f2a1630ed857d29278916a
alexbs01 <alex.becerra.suarez01@gmail.com>
2024-10-25 12:16:57 +0200

    TaskExecutor añadido

--- appOlimpiadas/backend/src/main/java/com/example/demo/services/log/LogService.java
+++ appOlimpiadas/backend/src/main/java/com/example/demo/services/log/LogService.java
@@ -3,19 +3,25 @@ package com.example.demo.services.log;
 import com.example.demo.entitites.log.Log;
 import com.example.demo.repository.log.LogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Service;
 
 @Service("logService")
 public class LogService {
+
+    private final LogRepository logRepository;
+    private final TaskExecutor taskExecutor;
+
     @Autowired
-    private LogRepository logRepository;
+    public LogService(LogRepository logRepository, @Qualifier("taskExecutor") TaskExecutor taskExecutor) {
+        this.logRepository = logRepository;
+        this.taskExecutor = taskExecutor;
+    }
 
     public void save(Log log) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                logRepository.save(log);
-            }
-        }).start();
+        Runnable runnable = () -> logRepository.save(log);
+        taskExecutor.execute(runnable);
     }
+
 }
